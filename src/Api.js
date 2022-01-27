65ecde50e04a97c1563a38ed961c352f6a2eea02
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2022-01-27 19:09:44 -0300

    ...

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,6 @@
+import env from "react-dotenv";
 import axios from "axios";
 
-export const api =  axios.create({
-    baseURL: `http://localhost:5000`
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
